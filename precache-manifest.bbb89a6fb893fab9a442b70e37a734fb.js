9cba5795e55e236e69a4d28940d7076616ff9f29
fakhrilak <fakhrilak23@gmail.com>
2020-10-26 05:01:01 +0700

    Updates

---
+++ precache-manifest.bbb89a6fb893fab9a442b70e37a734fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4f0dcc29570a6f94da6f6098e83693d",
+    "url": "/kafiraya-client/index.html"
+  },
+  {
+    "revision": "32d49bdee4749f71d10f",
+    "url": "/kafiraya-client/static/css/main.5e96a0c0.chunk.css"
+  },
+  {
+    "revision": "a158894e276e4f5deffe",
+    "url": "/kafiraya-client/static/js/2.89aa880e.chunk.js"
+  },
+  {
+    "revision": "4df4c58fd3db8af31000da1d75d059ea",
+    "url": "/kafiraya-client/static/js/2.89aa880e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32d49bdee4749f71d10f",
+    "url": "/kafiraya-client/static/js/main.79981087.chunk.js"
+  },
+  {
+    "revision": "b738da3e4c3e8f3b9560",
+    "url": "/kafiraya-client/static/js/runtime-main.21d2c8de.js"
+  },
+  {
+    "revision": "c1cc3d530d59c6ceaf55681cf4e3ce79",
+    "url": "/kafiraya-client/static/media/logo.c1cc3d53.png"
+  }
+]);
